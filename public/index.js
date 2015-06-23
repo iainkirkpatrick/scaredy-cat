2cf1b804ee920e9e472111afb18c905b2f1f00dd
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-06-23 15:20:32 +1200

    looking at react modifying CSS

--- public/index.js
+++ public/index.js
@@ -1,46 +1,15 @@
-// var _ = highland;
-//
-// var mouse = _('mousemove', document).map(function(m) {
-//   return m;
-// })
-//
-// mouse.each(function(m) {
-//   console.log(m);
-// });
-
-//array.map: it takes an array / list of things, applies an operation to each of them,
-//and returns a new list with each of the things transformed
-
-//so if i use array.map, i probably want to input a list of mousemove, and
-//get back a list of mouse x,y coords
-
-// var doubled = _([1, 2, 3, 4]).map(function (x) {
-//     return x * 2;
-// });
-//
-// doubled.each(function(n) {
-//   console.log(n);
-// });
-
-// $("body").mousemove(function(e) {
-//   console.log(e.pageX, e.pageY);
-// });
-
-
-// stream
-// subscribe to stream, function on event
-var bodyMouseStream = Rx.Observable.fromEvent($('body'), 'mousemove')
-  .map(function(e) {
-    return [e.pageX, e.pageY];
-  });
-
-bodyMouseStream.subscribe(function(coords) {
-  console.log(coords);
-});
+var catStyle = {
+  position: 'relative',
+  left: '100px',
+  top: '100px',
+};
 
 var Cat = React.createClass({
+  getInitialState: function() {
+    return {coords: [0,0]};
+  },
   render: function() {
-    return (<div id='cat'><img src={'cat.png'}/></div>)
+    return (<div style={catStyle} id='cat'><img src={'cat.png'}/></div>)
   }
 });
 
@@ -48,3 +17,13 @@ React.render(
   <Cat />,
   document.getElementById('container')
 );
+
+
+var bodyMouseStream = Rx.Observable.fromEvent($('html'), 'mousemove')
+  .map(function(e) {
+    return [e.pageX, e.pageY];
+  });
+
+bodyMouseStream.subscribe(function(coords) {
+  console.log(coords);
+});
