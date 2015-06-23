a070b89351cb8f6274a6c1453b941af0f6127b51
iainkirkpatrick <apprentice@StMac1.local>
2015-06-23 17:06:23 +1200

    component subscribing to the mousemove stream, updating position reactively

--- public/index.js
+++ public/index.js
@@ -1,14 +1,27 @@
-var catStyle = {
-  position: 'relative',
-  left: '100px',
-  top: '100px',
-};
+var bodyMouseStream = Rx.Observable.fromEvent($('html'), 'mousemove')
+  .map(function(e) {
+    return [e.pageX, e.pageY];
+  });
 
 var Cat = React.createClass({
   getInitialState: function() {
     return {coords: [0,0]};
   },
+  componentDidMount: function() {
+    self = this;
+    bodyMouseStream.subscribe(function(coords) {
+      console.log(coords);
+      self.setState({
+        coords: coords
+      });
+    });
+  },
   render: function() {
+    var catStyle = {
+      position: 'relative',
+      left: this.state.coords[0],
+      top: this.state.coords[1],
+    };
     return (<div style={catStyle} id='cat'><img src={'cat.png'}/></div>)
   }
 });
@@ -17,13 +30,3 @@ React.render(
   <Cat />,
   document.getElementById('container')
 );
-
-
-var bodyMouseStream = Rx.Observable.fromEvent($('html'), 'mousemove')
-  .map(function(e) {
-    return [e.pageX, e.pageY];
-  });
-
-bodyMouseStream.subscribe(function(coords) {
-  console.log(coords);
-});
