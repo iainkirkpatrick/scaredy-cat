893dd77acfaa01f348f0ead07221614e8298afd7
peterjacobson <peterjacobson.nz@gmail.com>
2015-07-20 16:00:28 +1200

    cat slowly chases mouse

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Victor = require('victor');
 
 var bodyMouseStream = Rx.Observable.fromEvent(document.querySelector('html'), 'mousemove')
   .map(function(e) {
+    console.log([e.pageX, e.pageY])
     return [e.pageX, e.pageY];
   });
 
@@ -13,7 +14,7 @@ var Cat = React.createClass({
   getInitialState: function() {
     return {
       path: {
-        coords: [0, 0],
+        coords: [100, 100],
         vector: new Victor(0, -1) //backwards-pointing vector, randomise later
       }
     };
@@ -22,7 +23,6 @@ var Cat = React.createClass({
     self = this;
     bodyMouseStream.subscribe(function(mouseCoords) {
       var pounceProximity = 100
-      console.log(self.state);
       var currentProximity = self.calculateDistanceBetween(mouseCoords, self.state.path.coords, catSize)
       if (currentProximity < pounceProximity) {
         self.pounce(mouseCoords)
@@ -32,8 +32,8 @@ var Cat = React.createClass({
   render: function() {
     var catStyle = {
       position: 'relative',
-      left: this.state.path.coords[0],
-      top: this.state.path.coords[1],
+      left: this.state.path.coords[0] - catSize/2,
+      top: this.state.path.coords[1] - catSize/2,
     };
     var imgStyle = {
       width: catSize + 'px',
@@ -46,21 +46,21 @@ var Cat = React.createClass({
     )
   },
   calculateDistanceBetween: function(mouseCoords, catCoords, catSize) {
-    console.log(catCoords)
-    var catCentreCoords = catCoords.map(function(e) {
-      return e + catSize/2
-    })
-    console.log(catCentreCoords);
-    var xDist = Math.abs(catCentreCoords[0] - mouseCoords[0])
-    var yDist = Math.abs(catCentreCoords[1] - mouseCoords[1])
+    // var catCentreCoords = catCoords.map(function(e) {
+    //   return e + catSize/2
+    // })
+    var xDist = Math.abs(catCoords[0] - mouseCoords[0])
+    var yDist = Math.abs(catCoords[1] - mouseCoords[1])
     var centreDistance = Math.sqrt(xDist * xDist + yDist * yDist);
     return centreDistance - catSize / 2;
   },
   pounce: function(mouseCoords) {
+    console.log(mouseCoords);
+    self = this;
     this.setState({
       path: {
-        coords: mouseCoords.map(function(e) {
-          return e - catSize / 2;
+        coords: mouseCoords.map(function(coord, i) {
+          return  self.state.path.coords[i] + (coord - self.state.path.coords[i]) / 10;
         }),
       } 
     }); 
